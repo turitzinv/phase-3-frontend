0385c3319bfe948a97fd8bf86be96b152b15a891
Victor Turitzin <turitzinv@gmail.com>
2022-06-01 15:20:45 -0400

    Installed react-router-dom@5. Imported BrowserRouter and created a NavBar with Navlinks. Imported NavBar to App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Button from '@mui/material/Button';
+import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
+
   return (
     <div>
-      <h1>Welcome!</h1>
-      <Button variant="contained" color="primary">Test Button</Button>
+      <h1>Test</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const linkStyles = {
+  display: "inline-block",
+  width: "150px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+function NavBar() {
+  return (
+    <div>
+      <NavLink to="/" style={linkStyles}>
+        Home
+      </NavLink>
+      <NavLink to="/librarylist" style={linkStyles}>
+        Library List
+      </NavLink>
+      <NavLink to="/createnewbook" style={linkStyles}>
+        Create New Book
+      </NavLink>
+    </div>
+  );
+}
+
+export default NavBar;
